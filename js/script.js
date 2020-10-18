3d7c0a1c1f8f753c3a130c845e833bdcd7db613a
k0xxx <androidk0xxx@gmail.com>
2020-10-18 12:46:57 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-let n = document.getElementById("text").value;
 let data = [];
-function add(date, time, text) {
-    data.push({ date: "date", time: "time", text: "text" });
+function addNewTask(date, time, text) {
+    data.push({ date: date, time: time, text: text });
     console.log("data is: ", data);
 }
-document.getElementById("btn").onclick = function () {
-    add("date", "time", n);
+document.getElementById("addTaskBtn").onclick = function () {
+    let date = document.getElementById("newTaskDate").value;
+    let time = document.getElementById("newTaskTime").value;
+    let text = document.getElementById("newTaskText").value;
+
+    addNewTask(date, time, text);
 };
