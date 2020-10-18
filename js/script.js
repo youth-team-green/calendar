93f775feee7d2a3ad6ecba193e4a19f8101aedd4
k0xxx <androidk0xxx@gmail.com>
2020-10-18 12:35:27 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-document.getElementById("btn").onclick = function(){add("date", "time", n)};
 let n = document.getElementById("text").value;
 let data = [];
 function add(date, time, text) {
-    data.push({date: "date", time: "time", text: "text"})
+    data.push({ date: "date", time: "time", text: "text" });
+    console.log("data is: ", data);
 }
-console.log("data is: ", data);
+document.getElementById("btn").onclick = function () {
+    add("date", "time", n);
+};
